4163dc66a4befedfc419bab6e4a2dcb8037143c3
heewon <rlagmldnjs88@gmail.com>
2025-12-30 20:00:25 +0900

    fix: 서버 컴포넌트에서 쿠키 set 에러 해결

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -17,8 +17,10 @@ export const createSupabaseServerClient = async () => {
             cookiesToSet.forEach(({ name, value, options }) => {
               cookieStore.set(name, value, options);
             });
-          } catch (e) {
-            console.error('Error setting cookies', e);
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
           }
         },
       },
